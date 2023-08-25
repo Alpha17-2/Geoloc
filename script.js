a1e90911e6f24cd57871b67f283f750dd2c0b2bc
Subhojeet Sahoo <57010722+Alpha17-2@users.noreply.github.com>
2023-08-25 08:51:30 +0530

    updated code

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ function initMap() {
     map = new Microsoft.Maps.Map(document.getElementById('map'), {
         credentials: 'AnCHimfxgLwemKyuJZYVq0Js4tBrDgI69aue-fYZg2xDvuhljPFFdK6qjNBdvD5U',
         center: new Microsoft.Maps.Location(12.9362, 77.6062), // Bangalore
-        zoom: 12
+        zoom: 15
     });
 
-    // Simulated user location (latitude and longitude)
+
     const userLatitude = 12.9362;
     const userLongitude = 77.6062;
     
-    // Add user's location marker
+
     const userLocation = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(userLatitude, userLongitude), {
-        color: 'blue'
+        color: 'Red'
     });
     map.entities.push(userLocation);
 
